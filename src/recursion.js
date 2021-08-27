c426e644c118128d4b46d926692eecd3d7b74600
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-08-27 17:21:29 -0400

    Updated sum function

--- src/recursion.js
+++ src/recursion.js
@@ -19,13 +19,13 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
-  if (array.length === 0){
-    return 0;
-  } else if (array.length === 1) {
-    return array[0];
-  } else {
-    return array[0] + sum(array.slice(1));
+  var _recurse = function(array, index) {
+    if (index < 0){
+      return 0;
+    }
+    return _recurse(array, index - 1) + array[index];
   }
+  return _recurse(array, array.length - 1);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
