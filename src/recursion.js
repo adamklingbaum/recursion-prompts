bacf7a1a121e87453cbc9c9d0ac1d30205262b8e
Adam Klingbaum <adam.klingbaum@gmail.com>
2021-08-28 00:38:21 -0400

    Implemented palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -140,6 +140,18 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string = string.toLowerCase().split(' ').join('');
+
+  if (string.length <= 1) {
+    return true;
+  }
+
+  if (string[0] === string[string.length - 1]) {
+    return palindrome(string.substring(1, string.length - 1));
+  }
+
+  return false;
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
